8a7092b4ac761a9713d0e77f8e489634d09875db
Moshe Brevda <mbrevda@gmail.com>
2011-09-09 12:36:45 +0000

    re #1426 - code/gui cosmetics

--- page.cdr.php
+++ page.cdr.php
@@ -282,8 +282,8 @@ if (isset($_POST['limit']) ) {
 <tr>
 <td>
 <select name="sort" id="sort">
-<option <?php if (isset($_POST['sort']) && $_POST['sort'] == 'ASC') { echo 'selected="selected"'; } ?> value="ASC"><?php echo _("Ascending")?></option>
-<option <?php if (empty($_POST['sort']) || $_POST['sort'] == 'DESC') { echo 'selected="selected"'; } ?> value="DESC"><?php echo _("Descending")?></option>
+<option <?php if (isset($_POST['sort']) && $_POST['sort'] == 'ASC') { echo 'selected="selected"'; } ?> value="ASC"><?php echo _("Oldest First")?></option>
+<option <?php if (empty($_POST['sort']) || $_POST['sort'] == 'DESC') { echo 'selected="selected"'; } ?> value="DESC"><?php echo _("Newest First")?></option>
 </select>
 </td>
 <td><table width="100%"><tr><td>
@@ -763,29 +763,3 @@ if ( isset($_POST['need_chart_cc']) && $_POST['need_chart_cc'] == 'true' ) {
 
 ?>
 </div>
-
-<script language='JavaScript'>
-<!-- Begin
-	function cdr_play(row_num, link) {
-	var i = 0;
-	var playbackId = "CURRENT_MSG";
-	var file=encodeURIComponent(link)
-	var cmTable = document.getElementById('cdr_table');
-	// Only one playback row is allowed to be open at a time.
-	// If one is already open, close it.
-	for (i = 0; i < cmTable.rows.length; i++) {
-		if (cmTable.rows[i].id == playbackId) {
-			// Delete the row; it's a Playback control row.
-			cmTable.deleteRow(cmTable.rows[i].rowIndex);
-		}
-	}
-	// Make our Playback row.
-	playback_src = "<iframe width='100%' height='25px' marginheight='0' marginwidth='0' frameborder='0' scrolling='no' src=" + link + "></iframe>";
-	newRow = cmTable.insertRow(row_num);
-	newRow.id = playbackId;
-	cell_left = newRow.insertCell(0);
-	cell_left.colSpan = 15;
-	cell_left.innerHTML = playback_src;
-	}
-// End -->
-</script>
