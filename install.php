85f389c0eabc9454e7d75578c7833981f0b3887e
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2012-04-25 16:20:54 +0000

    make sure install script all uses remote db for any migrations re #5788

--- install.php
+++ install.php
@@ -22,9 +22,29 @@ if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 //
 global $db;
 global $amp_conf;
+
 // Retrieve database and table name if defined, otherwise use FreePBX default
 $db_name = !empty($amp_conf['CDRDBNAME'])?$amp_conf['CDRDBNAME']:"asteriskcdrdb";
 $db_table_name = !empty($amp_conf['CDRDBTABLENAME'])?$amp_conf['CDRDBTABLENAME']:"cdr";
+
+// if CDRDBHOST and CDRDBTYPE are not empty then we assume an external connection and don't use the default connection
+//
+if (!empty($amp_conf["CDRDBHOST"]) && !empty($amp_conf["CDRDBTYPE"])) {
+	$db_hash = array('mysql' => 'mysql', 'postgres' => 'pgsql');
+	$db_type = $db_hash[$amp_conf["CDRDBTYPE"]];
+	$db_host = $amp_conf["CDRDBHOST"];
+	$db_port = empty($amp_conf["CDRDBPORT"]) ? '' :  ':' . $amp_conf["CDRDBPORT"];
+	$db_user = empty($amp_conf["CDRDBUSER"]) ? $amp_conf["AMPDBUSER"] : $amp_conf["CDRDBUSER"];
+	$db_pass = empty($amp_conf["CDRDBPASS"]) ? $amp_conf["AMPDBPASS"] : $amp_conf["CDRDBPASS"];
+	$datasource = $db_type . '://' . $db_user . ':' . $db_pass . '@' . $db_host . $db_port . '/' . $db_name;
+	$dbcdr = DB::connect($datasource); // attempt connection
+	if(DB::isError($dbcdr)) {
+		die_freepbx($dbcdr->getDebugInfo()); 
+	}
+} else {
+	$dbcdr = $db;
+}
+
 if (! function_exists("out")) {
         function out($text) {
                 echo $text."<br />";
@@ -32,12 +52,12 @@ if (! function_exists("out")) {
 }
 out(_("Checking if field did is present in cdr table.."));
 $sql = "SELECT did FROM $db_name.$db_table_name";
-$confs = $db->getRow($sql, DB_FETCHMODE_ASSOC);
+$confs = $dbcdr->getRow($sql, DB_FETCHMODE_ASSOC);
 if (DB::IsError($confs)) { // no error... Already there
   out(_("Adding did field to cdr"));
   out(_("This might take a while......"));
   $sql = "ALTER TABLE $db_name.$db_table_name ADD did VARCHAR ( 50 ) NOT NULL DEFAULT ''";
-  $results = $db->query($sql);
+  $results = $dbcdr->query($sql);
   if(DB::IsError($results)) {
     die($results->getMessage());
   }
@@ -48,11 +68,11 @@ if (DB::IsError($confs)) { // no error... Already there
 
 out(_("Checking if field recordingfile is present in cdr table.."));
 $sql = "SELECT recordingfile FROM $db_name.$db_table_name";
-$confs = $db->getRow($sql, DB_FETCHMODE_ASSOC);
+$confs = $dbcdr->getRow($sql, DB_FETCHMODE_ASSOC);
 if (DB::IsError($confs)) { // no error... Already there
     out(_("Adding recordingfile field to cdr"));
     $sql = "ALTER TABLE $db_name.$db_table_name ADD recordingfile VARCHAR ( 255 ) NOT NULL DEFAULT ''";
-    $results = $db->query($sql);
+    $results = $dbcdr->query($sql);
     if(DB::IsError($results)) {
         out(_('Unable to add recordingfile field to cdr table'));
         freepbx_log(FPBX_LOG_ERROR,"failed to add recordingfile field to cdr table");
