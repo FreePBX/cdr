e3e503567816b9cd96b8d0ecba631ddeb857acc8
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-03 18:12:57 -0500

    Update license info

--- functions.inc.php
+++ functions.inc.php
@@ -1,26 +1,10 @@
 <?php
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
-//This file is part of FreePBX.
+//	License for all code of this FreePBX module can be found in the license file inside the module directory
+//  Portions Copyright (C) 2011 Igor Okunev
+//  Portions Copyright (C) 2011 Mikael Carlsson
+//	Copyright 2013 Schmooze Com Inc.
 //
-//    FreePBX is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 2 of the License, or
-//    (at your option) any later version.
-//
-//    FreePBX is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with FreePBX.  If not, see <http://www.gnu.org/licenses/>.
-//
-//    cdr module for FreePBX 2.7+
-//    Copyright (C) 2010, 2011 Anthony Joseph Messina
-//    Portions Copyright (C) 2011 Igor Okunev
-//    Portions Copyright (C) 2011 Mikael Carlsson
-//    Portions Copyright (C) 2006 Seth Sargent, Steven Ward
-
 class cdr_conf {
 	var $_cel_general = array();
 
