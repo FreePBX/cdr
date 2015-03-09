f9e99713fb08ce13eb6b3170cf8d54f5e82f910b
Rob Thomas <xrobau@gmail.com>
2015-03-10 07:10:56 +1000

    FREEPBX-8845 - SQL Injection Vulnerability

--- page.cdr.php
+++ page.cdr.php
@@ -6,7 +6,7 @@
 //
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 
-global $amp_conf;
+global $amp_conf, $db;
 // Are a crypt password specified? If not, use the supplied.
 $REC_CRYPT_PASSWORD = (isset($amp_conf['AMPPLAYKEY']) && trim($amp_conf['AMPPLAYKEY']) != "")?trim($amp_conf['AMPPLAYKEY']):'TheWindCriesMary';
 $dispnum = "cdr";
@@ -62,7 +62,9 @@ switch ($action) {
 		break;
 }
 
-
+foreach ($_POST as $k => $v) {
+	$_POST[$k] = preg_replace('/;/', ' ', $dbcdr->escapeSimple($v);
+}
 
 $h_step = 30;
 ?>
@@ -440,10 +442,6 @@ if ($amp_conf['CEL_ENABLED'] && !isset($_POST['need_html']) && $action == 'cel_s
 }
 
 echo '<a id="CDR"></a>';
-foreach ( array_keys($_POST) as $key ) {
-	$_POST[$key] = preg_replace('/;/', ' ', $_POST[$key]);
-	$_POST[$key] = $db->escapeSimple($_POST[$key]);
-}
 
 $startmonth = empty($_POST['startmonth']) ? date('m') : $_POST['startmonth'];
 $startyear = empty($_POST['startyear']) ? date('Y') : $_POST['startyear'];
