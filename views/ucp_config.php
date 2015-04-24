3ede93e9ff57878bf5b95f5114cc1265c7c3e6f1
Andrew Nagy <andrew.nagy@the159.com>
2015-04-23 20:03:41 -0400

    formatting

--- views/ucp_config.php
+++ views/ucp_config.php
@@ -4,8 +4,8 @@
 			<div class="row">
 				<div class="form-group">
 					<div class="col-md-3">
-						<label class="control-label" for="cdr_download"><?php echo _("Allow CDR")?></label>
-						<i class="fa fa-question-circle fpbx-help-icon" data-for="cdr_download"></i>
+						<label class="control-label" for="cdr_enable"><?php echo _("Allow CDR")?></label>
+						<i class="fa fa-question-circle fpbx-help-icon" data-for="cdr_enable"></i>
 					</div>
 					<div class="col-md-9">
 						<span class="radioset">
