b4756bfb573c1afe4ee729f865d6c05bb3654e65
James Finstrom <jfinstrom@sangoma.com>
2019-01-10 16:30:54 +0000

    FREEPBX-18883 don't hardcode binary locations in restore class

--- Restore.php
+++ Restore.php
@@ -9,11 +9,11 @@ class Restore Extends Base\RestoreBase{
     $dbhandle = $this->FreePBX->Cdr->getCdrDbHandle();
     if (file_exists($dumpfile)) {
         $dbhandle->query('TRUNCATE cdr');
-        $command = sprintf('/usr/bin/gunzip -d %s', $dumpfile);
+        $command = sprintf('%s -d %s', fpbx_which('gzip'),$dumpfile);
         $gunzip = new Process($command);
         $gunzip->mustRun();
         $newfilename = substr($dumpfile, 0, -3);
-        $restore = 'mysql asteriskcdrdb -e "LOAD DATA INFILE \''.$newfilename.'\' INTO TABLE cdr;"';
+        $restore = fpbx_which('mysql').' asteriskcdrdb -e "LOAD DATA INFILE \''.$newfilename.'\' INTO TABLE cdr;"';
         $sql = new Process($restore);
         $sql->mustRun();
         return true;
