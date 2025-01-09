b1fc5b2db60e78ce70f39380464f4e26486e3e12
Rahul Verma <rverma@sangoma.com>
2025-01-09 03:22:05 +0000

    FREEI-1575 Scribe icon issue in UCP

--- ucp/assets/js/global.js
+++ ucp/assets/js/global.js
@@ -43,8 +43,8 @@ var CdrC = UCPMC.extend({
 			return '';
 		}
 		var link = '<a class="download" alt="'+_("Download")+'" href="'+UCP.ajaxUrl+'?module=cdr&amp;command=download&amp;msgid='+row.uniqueid+'&amp;type=download&amp;ext='+row.requestingExtension+'"><i class="fa fa-cloud-download"></i></a>';
-		if(row.converttotext !== undefined && row.converttotext !== null && row.converttotext != '' && settings.isScribeEnabled) {
-			link += '<a href="#" class="transcript tool-tip" data-toggle="tooltip" title="Read the voice transcription" onclick="openmodal(\'' + UCP.ajaxUrl+row.converttotext + '\')"> <img src="../admin/assets/scribe/images/scribe.png" width="15px" height="15px" alt="PBX Scribe" /></a>';
+		if(row.converttotext.transcriptionURL !== undefined && row.converttotext.transcriptionURL !== null && row.converttotext.transcriptionURL != '' && settings.isScribeEnabled) {
+			link += '<a href="#" class="transcript tool-tip" data-toggle="tooltip" title="Read the voice transcription" onclick="openmodal(\'' + UCP.ajaxUrl+row.converttotext.transcriptionURL + '\')"> <img src="'+row.converttotext.scribeIconURL+'" width="15px" height="15px" alt="PBX Scribe" /></a>';
 		}
 		return link;
 	},
