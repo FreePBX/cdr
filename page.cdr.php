b76466e5e0b709caddd2df27650c4d45b0caeb37
kapil gupta <kgupta@sangoma.com>
2026-02-23 10:37:01 +0000

    FREEI-2679 fix:add sanitization for limit POST parameter

--- page.cdr.php
+++ page.cdr.php
@@ -559,7 +559,9 @@ $mod_vars['userfield'][] = empty($_POST['userfield_neg']) ? NULL : $_POST['userf
 $mod_vars['accountcode'][] = !isset($_POST['accountcode']) ? NULL : $_POST['accountcode'];
 $mod_vars['accountcode'][] = empty($_POST['accountcode_mod']) ? NULL : $_POST['accountcode_mod'];
 $mod_vars['accountcode'][] = empty($_POST['accountcode_neg']) ? NULL : $_POST['accountcode_neg'];
-$result_limit = (!isset($_POST['limit']) || empty($_POST['limit'])) ? $db_result_limit : $_POST['limit'];
+// Sanitize result limit to prevent SQL injection - cast to integer and enforce bounds
+$raw_limit = (isset($_POST['limit']) && $_POST['limit'] !== '') ? $_POST['limit'] : $db_result_limit;
+$result_limit = max(1, min((int) $raw_limit, 100000));
 
 $multi = array('dst', 'cnum', 'outbound_cnum');
 foreach ($mod_vars as $key => $val) {
